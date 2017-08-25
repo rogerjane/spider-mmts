7f9b2211527891f21d84bc9d2274cd5a60e43563
thickycat <dev@rog.cc>
2017-08-25 16:46:35 +0100

    Removal of original fileset

--- support/include/heapstrings.h
+++
@@ -1,7 +0,0 @@
-void szDelete(const char *sz);
-char *strappend(const char *szOriginal, const char *szAppend);
-char *strnappend(char *szOriginal, const char *szAppend, int nMaxLen);
-char *hprintf(const char *szOrig, const char *szFmt, ...);
-const char *strset(const char **pszOriginal, const char *szText);
-const char *strsubst(const char *szStr, const char *szSearch, const char *szReplace);
-const char *hReadFileLine(FILE *fp);
